fc0cd4df97a517a02e0679c34a501d0067ce1ddc
Jacob Potter <jacob@durbatuluk.us>
2013-01-08 20:56:49 -0800

    cleanup

--- render.c
+++ render.c
@@ -7,26 +7,24 @@
 
 const float nurbs_t_knots[] = { 0, 0, 0, 0.5, 1, 1, 1 };
 
-struct nurbs_patch *make_move(int npts,
-                              const struct nurbs_point *pts,
-                              const float *knots,
+struct nurbs_patch *make_move(const struct nurbs_line *line,
                               struct xy path[NURBS_T_POINTS]) {
 
 	size_t size = sizeof (struct nurbs_patch)
-	            + sizeof (struct nurbs_point) * npts * 4;
+	            + sizeof (struct nurbs_point) * line->points * 4;
 
 	struct nurbs_patch *out = malloc(size);
 	assert(out);
 
-	out->points = npts;
-	out->xy_knots = knots;
+	out->points = line->points;
+	out->xy_knots = line->knots;
 	out->t_knots = nurbs_t_knots;
 
-	for (int i = 0; i < npts; i++) {
+	for (int i = 0; i < line->points; i++) {
 		for (int j = 0; j < 4; j++) {
-			out->t[i][j].x = pts[i].x + path[j].x;
-			out->t[i][j].y = pts[i].y + path[j].y;
-			out->t[i][j].weight = pts[i].weight;
+			out->t[i][j].x = line->t[i].x + path[j].x;
+			out->t[i][j].y = line->t[i].y + path[j].y;
+			out->t[i][j].weight = line->t[i].weight;
 		}
 	}
 
@@ -54,11 +52,11 @@ struct xy move_path[][4] = {
 struct nurbs_patch *moving_circles[12];
 
 void render_init(void) {
-	struct nurbs_line *line = nurbs_load_line("data/circle.nub");
+	struct nurbs_line *line = nurbs_load_line("data/square.nub");
 	assert(line);
 
 	for (int i = 0; i < 12; i++)
-		moving_circles[i] = make_move(line->points, line->t, line->knots, move_path[i]);
+		moving_circles[i] = make_move(line, move_path[i]);
 }
 
 struct xy render_point(float u, float per_circle) {
